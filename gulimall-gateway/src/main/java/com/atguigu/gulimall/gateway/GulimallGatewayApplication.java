f78e3ef22305c54332f2880b93665dddfee49217
Archer-Fant <1091053002@qq.com>
2021-06-05 23:38:17 +0800

    谷粒基础篇第一个部分完成

--- gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/GulimallGatewayApplication.java
+++ gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/GulimallGatewayApplication.java
@@ -2,8 +2,10 @@ package com.atguigu.gulimall.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GulimallGatewayApplication {
 
     public static void main(String[] args) {
